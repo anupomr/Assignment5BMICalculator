0251b6789d083fe5bc895d342dca1a93c80b7483
Anupom Roy <aroy34@my.centennialcollege.ca>
2017-08-04 16:28:41 -0400

    Created Demo Application

---
+++ Assignment5BMICalculator/BMICalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+Name: Anupom Roy
+ID  :300853516
+Date: August 3, 2017
+Description: Created Demo Application
+Version: 0.1
+ */
+namespace Assignment5BMICalculator
+{
+    public partial class BMICalculator : Form
+    {
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Assignment5BMICalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+Name: Anupom Roy
+ID  :300853516
+Date: August 3, 2017
+Description: A BMICalculator app with Windows Forms
+Version: 0.1
+ */
+namespace Assignment5BMICalculator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BMICalculator());
+        }
+    }
+}
